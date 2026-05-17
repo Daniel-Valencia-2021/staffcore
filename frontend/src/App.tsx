8878381e2222cd7ee30dc1ee7a01c3a277df12d9
Daniel Alexis Valencia Nieves <davalencia616@gmail.com>
2026-05-17 15:06:59 -0500

    chore(frontend): initialize shadcn/ui with Radix and Luma preset

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import Dashboard from './pages/Dashboard'
+import Empleados from './pages/Empleados'
+import Departamentos from './pages/Departamentos'
+import Nomina from './pages/Nomina'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <h1>Hello, StaffCore</h1>
+  )
+}
+
+export default App
